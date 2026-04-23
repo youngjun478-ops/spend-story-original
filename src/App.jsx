9b1ea64cc5194e9771a16e54b1d376a788601085
youngjun478-ops <youngjun478@gmail.com>
2026-04-24 05:20:09 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-export default function ViralMVP() {
+import React from "react";
+
+function App() {
   return (
-    <div>...</div>
+    <div style={{padding:40}}>
+      <h1>Spend Story 정상 실행</h1>
+      <p>이 화면이 보이면 배포 성공</p>
+    </div>
   );
 }
+
+export default App;
